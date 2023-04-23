65ae82eac845927f9e9dd65ede5b15b37475d0a8
Ashok GC <78782373+Ashok-gc@users.noreply.github.com>
2023-04-23 22:10:20 +0545

    one side lane detected correctly

--- another3.py
+++ another3.py
@@ -94,27 +94,62 @@
 import cv2
 import numpy as np
 
-def grayscale(image):
-    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def grayscale(img):
+    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-def canny(image, low_threshold, high_threshold):
-    return cv2.Canny(image, low_threshold, high_threshold)
+def gaussian_blur(img, kernel_size):
+    return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
 
-def region_of_interest(image, vertices):
-    mask = np.zeros_like(image)
+def canny(img, low_threshold, high_threshold):
+    return cv2.Canny(img, low_threshold, high_threshold)
+
+def region_of_interest(img, vertices):
+    mask = np.zeros_like(img)
     cv2.fillPoly(mask, vertices, 255)
-    masked_image = cv2.bitwise_and(image, mask)
-    return masked_image
+    return cv2.bitwise_and(img, mask)
 
-def hough_lines(image, rho, theta, threshold, min_line_length, max_line_gap):
-    lines = cv2.HoughLinesP(image, rho, theta, threshold, np.array([]), minLineLength=min_line_length, maxLineGap=max_line_gap)
-    return lines
+def hough_lines(img, rho, theta, threshold, min_line_length, max_line_gap):
+    return cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), min_line_length, max_line_gap)
 
+# def draw_lines(image, lines, left_color, right_color, thickness):
+#     left_lines = []
+#     right_lines = []
+#     min_slope = 0.5
+#     max_slope = 2.0
+
+#     for line in lines:
+#         for x1, y1, x2, y2 in line:
+#             if x2 == x1:
+#                 continue
+#             slope = (y2 - y1) / (x2 - x1)
+#             intercept = y1 - slope * x1
+#             if min_slope < abs(slope) < max_slope:
+#                 if slope < 0:
+#                     left_lines.append((slope, intercept))
+#                 else:
+#                     right_lines.append((slope, intercept))
+
+#     y1 = int(image.shape[0] * 0.6)
+#     y2 = image.shape[0]
+#     y = np.array([y1, y2])
+
+#     def draw_lane_lines(lines, color):
+#         if lines:
+#             slope, intercept = np.mean(lines, axis=0)
+#             x1, x2 = (y - intercept) / slope
+#             x1, x2 = int(x1), int(x2)
+#             cv2.line(image, (x1, y1), (x2, y2), color, thickness)
+
+#     draw_lane_lines(left_lines, left_color)
+#     draw_lane_lines(right_lines, right_color)
 def draw_lines(image, lines, left_color, right_color, thickness):
-    left_line_points = []
-    right_line_points = []
-    left_weights = []
-    right_weights = []
+    if lines is None:
+        return
+
+    left_lines = []
+    right_lines = []
+    min_slope = 0.5
+    max_slope = 2.0
 
     for line in lines:
         for x1, y1, x2, y2 in line:
@@ -122,80 +157,83 @@ def draw_lines(image, lines, left_color, right_color, thickness):
                 continue
             slope = (y2 - y1) / (x2 - x1)
             intercept = y1 - slope * x1
-            length = np.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2)
-            if slope < 0:
-                left_line_points.append((slope, intercept))
-                left_weights.append(length)
-            else:
-                right_line_points.append((slope, intercept))
-                right_weights.append(length)
-
-    left_lane = np.dot(left_weights, left_line_points) / np.sum(left_weights) if len(left_weights) > 0 else None
-    right_lane = np.dot(right_weights, right_line_points) / np.sum(right_weights) if len(right_weights) > 0 else None
+            if min_slope < abs(slope) < max_slope:
+                if slope < 0:
+                    left_lines.append((slope, intercept))
+                else:
+                    right_lines.append((slope, intercept))
 
     y1 = int(image.shape[0] * 0.6)
     y2 = image.shape[0]
+    y = np.array([y1, y2])
 
-    def calc_x(y, line):
-        return int((y - line[1]) / line[0])
+    def draw_lane_lines(lines, color):
+        if lines:
+            slope, intercept = np.mean(lines, axis=0)
+            x1, x2 = (y - intercept) / slope
+            x1, x2 = int(x1), int(x2)
+            cv2.line(image, (x1, y1), (x2, y2), color, thickness)
 
-    if left_lane is not None:
-        left_x1, left_x2 = calc_x(y1, left_lane), calc_x(y2, left_lane)
-        cv2.line(image, (left_x1, y1), (left_x2, y2), left_color, thickness)
+    draw_lane_lines(left_lines, left_color)
+    draw_lane_lines(right_lines, right_color)
 
-    if right_lane is not None:
-        right_x1, right_x2 = calc_x(y1, right_lane), calc_x(y2, right_lane)
-        cv2.line(image, (right_x1, y1), (right_x2, y2), right_color, thickness)
-
-    if left_lane is not None and right_lane is not None:
-        cv2.fillPoly(image, [np.array([(left_x1, y1), (left_x2, y2), (right_x2, y2), (right_x1, y1)])], (0, 255, 0))
 
 def process_image(image, parameters):
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    yellow_mask = cv2.inRange(hsv, np.array([20, 100, 100]), np.array([40, 255, 255]))
+    white_mask = cv2.inRange(hsv, np.array([0, 0, 200]), np.array([180, 30, 255]))
+    color_mask = cv2.bitwise_or(yellow_mask, white_mask)
+    
     gray = grayscale(image)
-    edges = canny(gray, parameters["canny_low"], parameters["canny_high"])
-    masked_edges = region_of_interest(edges, parameters["roi_vertices"])
-    lines = hough_lines(masked_edges, parameters["hough_rho"], parameters["hough_theta"], parameters["hough_threshold"], parameters["min_line_length"], parameters["max_line_gap"])
+    blurred_gray = gaussian_blur(gray, parameters["gaussian_kernel"])
+    edges = canny(blurred_gray, parameters["canny_low"], parameters["canny_high"])
+    color_edges = cv2.bitwise_and(edges, color_mask)
+    masked_edges = region_of_interest(color_edges, parameters["roi_vertices"])
+    lines = hough_lines(masked_edges, parameters["hough_rho"],
+        parameters["hough_theta"], parameters["hough_threshold"], parameters["min_line_length"], parameters["max_line_gap"])
     line_image = np.copy(image)
     draw_lines(line_image, lines, parameters["left_color"], parameters["right_color"], parameters["line_thickness"])
-    
+
     return line_image
 
 def main(input_video, output_video, parameters):
     cap = cv2.VideoCapture(input_video)
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    out = cv2.VideoWriter(output_video, fourcc, 20.0, (frame_width, frame_height))
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(output_video, fourcc, 30.0, (int(cap.get(3)), int(cap.get(4))))
 
     while(cap.isOpened()):
         ret, frame = cap.read()
-        if ret:
-            result = process_image(frame, parameters)
-            out.write(result)
-            cv2.imshow('Lane Detection', result)
+        if not ret:
+            break
 
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        else:
+        processed_frame = process_image(frame, parameters)
+        out.write(processed_frame)
+
+        cv2.imshow('frame', processed_frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     cap.release()
     out.release()
     cv2.destroyAllWindows()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    input_video = 'project_video.mp4'
+    output_video = 'output_video.mp4'
+
     parameters = {
+        "gaussian_kernel": 5,
         "canny_low": 50,
         "canny_high": 150,
-        "roi_vertices": np.array([[(100, 540), (460, 325), (520, 325), (900, 540)]], dtype=np.int32),
-        "hough_rho": 1,
-        "hough_theta": np.pi/180,
-        "hough_threshold": 15,
-        "min_line_length": 40,
+        "roi_vertices": np.array([[(100, 540), (960, 540), (650, 350), (320, 350)]], dtype=np.int32),
+        "hough_rho": 2,
+        "hough_theta": np.pi / 180,
+        "hough_threshold": 40,
+        "min_line_length": 30,
         "max_line_gap": 20,
         "left_color": (255, 0, 0),
         "right_color": (0, 0, 255),
-        "line_thickness": 5
+        "line_thickness": 8,
     }
 
-    main('clip.mp4', 'output_video.avi', parameters)
+    main(input_video, output_video, parameters)
