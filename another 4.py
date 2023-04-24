7e6f9d4c23cc4786a34104d21640deb761d2b939
Ashok GC <78782373+Ashok-gc@users.noreply.github.com>
2023-04-24 08:26:31 +0545

    new lane detection

--- another 4.py
+++ another 4.py
@@ -1,106 +1,182 @@
-import cv2
 import numpy as np
+import cv2
+import pickle
+import glob
+from tracker import tracker
+from moviepy.editor import VideoFileClip
+from IPython.display import HTML
 
-def preprocess_frame(frame):
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-    canny = cv2.Canny(blurred, 50, 150)
-    return canny
+dist_pickle = pickle.load(open("calibration_pickle.p", "rb"))
+mtx = dist_pickle["mtx"]
+dist = dist_pickle["dist"]
 
-def detect_lane_lines(frame):
-    lines = cv2.HoughLinesP(frame, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
-    left_lane = []
-    right_lane = []
-
-    for line in lines:
-        x1, y1, x2, y2 = line.reshape(4)
-        parameters = np.polyfit((x1, x2), (y1, y2), 1)
-        slope = parameters[0]
-        intercept = parameters[1]
-
-        if slope < 0:
-            left_lane.append((slope, intercept))
-        else:
-            right_lane.append((slope, intercept))
-
-    left_lane_avg = np.average(left_lane, axis=0)
-    right_lane_avg = np.average(right_lane, axis=0)
-
-    return left_lane_avg, right_lane_avg
-
-def create_line_coordinates(frame, parameters):
-    slope, intercept = parameters
-    y1 = frame.shape[0]
-    y2 = int(y1 * (3/5))
-    x1 = int((y1 - intercept) / slope)
-    x2 = int((y2 - intercept) / slope)
-
-    return np.array([x1, y1, x2, y2])
-
-def track_lanes(frame, left_lane, right_lane):
-    left_line = create_line_coordinates(frame, left_lane)
-    right_line = create_line_coordinates(frame, right_lane)
-
-    return np.array([left_line, right_line])
-
-def color_lane_boundaries(frame, lines):
-    line_image = np.zeros_like(frame)
-    if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line
-            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
-
-    green_overlay = np.zeros_like(frame)
-    polygon = np.array([[
-        [lines[0][0], lines[0][1]],
-        [lines[0][2], lines[0][3]],
-        [lines[1][2], lines[1][3]],
-        [lines[1][0], lines[1][1]]
-    ]], dtype=np.int32)
-    cv2.fillPoly(green_overlay, polygon, (0, 255, 0))
-    combined_image = cv2.addWeighted(frame, 0.8, green_overlay, 0.2, 0)
-    return cv2.addWeighted(combined_image, 0.8, line_image, 1, 0)
-
-
-def lane_departure_warning(lines, frame_width):
-    left_lane, right_lane = lines
-    left_bottom_x = left_lane[0]
-    right_bottom_x = right_lane[0]
+def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0,255)):
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    if orient == 'x':
+        abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 1, 0))
+    if orient == 'y':
+        abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 0, 1))
     
-    lane_center = (right_bottom_x - left_bottom_x) // 2 + left_bottom_x
-    vehicle_position = frame_width // 2
-    threshold = frame_width * 0.05
-
-    if abs(lane_center - vehicle_position) > threshold:
-        return "WARNING: Lane Departure"
-    else:
-        return ""
-
-def main():
-    cap = cv2.VideoCapture('project_video.mp4')
-    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
+    binary_output = np.zeros_like(scaled_sobel)
     
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
+    binary_output[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 1
+    return binary_output
 
-        preprocessed_frame = preprocess_frame(frame)
-        left_lane, right_lane = detect_lane_lines(preprocessed_frame)
-        tracked_lanes = track_lanes(frame, left_lane, right_lane)
-        colored_frame = color_lane_boundaries(frame, tracked_lanes)
-        
-        lane_warning = lane_departure_warning(tracked_lanes, frame_width)
-        if lane_warning:
-            cv2.putText(colored_frame, lane_warning, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+def mag_thresh(img, sobel_kernel=3, mag_thresh=(0,255)):
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    
+    gradmag = np.sqrt(sobelx**2+sobely**2)
+    scale_factor = np.max(gradmag)/255
+    gradmag = (gradmag/scale_factor).astype(np.uint8)
+    
+    binary_output = np.zeros_like(gradmag)
+    binary_output[(gradmag >= mag_thresh[0]) & (gradmag <= mag_thresh[1])] = 1
+    return binary_output
+    
+def dir_threshold(image, sobel_kernel=3, thresh=(0, np.pi/2)):
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    
+    with np.errstate(divide='ignore', invalid='ignore'):
+        absgraddir = np.absolute(np.arctan(sobely/sobelx))
+        binary_output = np.zeros_like(absgraddir)
+        binary_output[(absgraddir >= thresh[0]) & (absgraddir <= thresh[1])] = 1
+    return binary_output
 
-        cv2.imshow('Lane Detection', colored_frame)
+def color_threshold(image, sthresh=(0,255), vthresh=(0,255), lthresh=(0,255)):
+    hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+    s_channel = hls[:,:,2]
+    s_binary = np.zeros_like(s_channel)
+    s_binary[(s_channel >= sthresh[0]) & (s_channel <= sthresh[1])] = 1
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    v_channel = hsv[:,:,2]
+    v_binary = np.zeros_like(v_channel)
+    v_binary[(v_channel >= vthresh[0]) & (v_channel <= vthresh[1])] = 1
 
-    cap.release()
-    cv2.destroyAllWindows()
+    output = np.zeros_like(s_channel)
+    output[(s_binary == 1) & (v_binary == 1)] = 1
+    return output
+    
+def window_mask(width, height, img_ref, center, level):
+    output = np.zeros_like(img_ref)
+    output[int(img_ref.shape[0] - (level + 1) * height):int(img_ref.shape[0] - level * height),
+    max(0, int(center - width / 2)):min(int(center + width / 2), img_ref.shape[1])] = 1
+    return output
 
-if __name__ == '__main__':
-    main()
+def process_image(image):
+
+    img = cv2.undistort(image, mtx, dist, None, mtx)
+
+    preprocessImage = np.zeros_like(img[:,:,0])
+    gradx = abs_sobel_thresh(img, orient='x', thresh=(12,255))
+    grady = abs_sobel_thresh(img, orient='x', thresh=(25,255))
+    c_binary = color_threshold(img, sthresh=(100,255),vthresh=(50,255))
+    preprocessImage[((gradx==1)&(grady==1)|(c_binary==1))] = 255
+
+    img_size = (img.shape[1], img.shape[0])
+    bot_width = .75 #changed from .76
+    mid_width = .1 #changed this value - seemed to work a lot better than 0.08 
+    height_pct = .62
+    bottom_trim = .935
+    src = np.float32([[img.shape[1]*(.5-mid_width/2),img.shape[0]*height_pct],[img.shape[1]*(.5+mid_width/2),img.shape[0]*height_pct],
+                      [img.shape[1]*(.5+bot_width/2),img.shape[0]*bottom_trim],[img.shape[1]*(.5-bot_width/2),img.shape[0]*bottom_trim]])
+    offset = img_size[0]*.25
+    dst = np.float32([[offset, 0], [img_size[0]-offset, 0],[img_size[0]-offset, img_size[1]],[offset, img_size[1]]])
+
+    M = cv2.getPerspectiveTransform(src, dst) 
+    Minv = cv2.getPerspectiveTransform(dst, src)
+    warped = cv2.warpPerspective(preprocessImage, M, img_size,flags=cv2.INTER_LINEAR)
+
+    window_width = 25
+    window_height = 80
+    curve_centers = tracker(Mywindow_width=window_width,Mywindow_height=window_height,Mymargin=25,My_ym=10/720,My_xm=4/384,Mysmooth_factor=15)
+    window_centroids = curve_centers.find_window_centroids(warped)
+
+    l_points = np.zeros_like(warped)
+    r_points = np.zeros_like(warped)
+
+    rightx = []
+    leftx = []
+
+    for level in range(0,len(window_centroids)):
+        l_mask = window_mask(window_width,window_height,warped,window_centroids[level][0],level)
+        r_mask = window_mask(window_width,window_height,warped,window_centroids[level][1],level)
+
+        leftx.append(window_centroids[level][0]) 
+        rightx.append(window_centroids[level][1])
+
+        l_points[(l_points==255)|((l_mask==1))] = 255
+        r_points[(r_points==255)|((r_mask==1))] = 255
+
+    template = np.array(r_points+l_points,np.uint8)
+    zero_channel = np.zeros_like(template)
+    template = np.array(cv2.merge((zero_channel,template,zero_channel)),np.uint8)
+    warpage = np.array(cv2.merge((warped,warped,warped)),np.uint8)
+    result = cv2.addWeighted(warpage,1,template,0.5,0.0)
+
+    yvals = range(0,warped.shape[0])
+    res_yvals = np.arange(warped.shape[0]-(window_height/2),0,-window_height)
+
+    left_fit = np.polyfit(res_yvals,leftx,2)
+    left_fitx = left_fit[0]*yvals*yvals + left_fit[1]*yvals + left_fit[2]
+    left_fitx = np.array(left_fitx,np.int32)
+
+    right_fit = np.polyfit(res_yvals,rightx,2)
+    right_fitx = right_fit[0]*yvals*yvals + right_fit[1]*yvals + right_fit[2]
+    right_fitx = np.array(right_fitx,np.int32)
+
+    left_lane = np.array(list(zip(np.concatenate((left_fitx-window_width/2,left_fitx[::-1]+window_width/2),axis=0),np.concatenate((yvals,yvals[::-1]),axis=0))),np.int32)
+
+    right_lane = np.array(list(zip(np.concatenate((right_fitx-window_width/2,right_fitx[::-1]+window_width/2),axis=0),np.concatenate((yvals,yvals[::-1]),axis=0))),np.int32)
+
+    inner_lane = np.array(list(zip(np.concatenate((left_fitx+window_width/2,right_fitx[::-1]+window_width/2),axis=0),np.concatenate((yvals,yvals[::-1]),axis=0))),np.int32)
+
+    road = np.zeros_like(img)
+    road_bkg = np.zeros_like(img)
+    cv2.fillPoly(road,[left_lane],color=[255,0,0])
+    cv2.fillPoly(road,[inner_lane],color=[0,255,0])
+    cv2.fillPoly(road,[right_lane],color=[0,0,255])
+    cv2.fillPoly(road_bkg,[left_lane],color=[255,255,255])
+    cv2.fillPoly(road_bkg,[right_lane],color=[255,255,255])
+
+    road_warped = cv2.warpPerspective(road,Minv,img_size,flags=cv2.INTER_LINEAR)
+    road_warped_bkg = cv2.warpPerspective(road_bkg,Minv,img_size,flags=cv2.INTER_LINEAR)
+
+    base = cv2.addWeighted(img, 1.0, road_warped_bkg, -1.0, 0.0)
+    result = cv2.addWeighted(base,1.0,road_warped,0.7,0.0)
+
+    #measure pixels in y and x directions
+    ym_per_pix = curve_centers.ym_per_pix
+    xm_per_pix = curve_centers.xm_per_pix
+
+    curve_fit_cr = np.polyfit(np.array(res_yvals,np.float32)*ym_per_pix,np.array(leftx,np.float32)*xm_per_pix,2)
+    curverad = ((1+(2*curve_fit_cr[0]*yvals[-1]*ym_per_pix+curve_fit_cr[1])**2)**1.5)/np.absolute(2*curve_fit_cr[0]) #remember that it's the equation from the lesson (derivatives) - radius of curvature
+
+    camera_center = (left_fitx[-1] + right_fitx[-1])/2
+    center_diff = (camera_center-warped.shape[1]/2)*xm_per_pix
+    side_pos = 'left'
+    if center_diff <= 0:
+        side_pos = 'right'
+
+    cv2.putText(result,'Radius of curvature = '+str(round(curverad,3))+'(m)',(50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),2)
+    cv2.putText(result,'Vehicle is '+str(abs(round(center_diff,3)))+'m '+side_pos+' of center',(50,100), cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),2)
+
+        # Display the result in a popup window
+    cv2.imshow('Detected Lanes', result)
+    cv2.waitKey(1)
+
+
+    return result
+    
+input_vid = 'project_video.mp4'
+output_vid = 'lane_tracker1.mp4'
+
+clip1 = VideoFileClip(input_vid)
+video_clip = clip1.fl_image(process_image)
+video_clip.write_videofile(output_vid, audio=False)
+cv2.destroyAllWindows()
